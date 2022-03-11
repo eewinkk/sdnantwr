bffb37ddbff72b6cbd802ad1eb9e84cde875f310
Erwin A Latif <eewinkk@gmail.com>
2022-03-11 08:45:29 +0700

    Update settings.py

--- sdnantwr/settings.py
+++ sdnantwr/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+'''
 DATABASES = {
     'default': {
         #MariaDB, MySQL, Oracle, or PostgreSQ
@@ -100,7 +100,7 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '3306',
     }
-}
+}'''
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
